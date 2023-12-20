a278f7b600daf94602877ba2a2a45f964a379bdc
Tom Kimpson <tomkimpson@gmail.com>
2023-12-21 10:31:28 +1100

    edits to process file

--- data/process_json_file.py
+++ data/process_json_file.py
@@ -19,3 +19,16 @@ def process_results_file(path):
 
     #Create a numpy array of the variables you want to plot
     y_post = df_posterior[variables_to_plot].to_numpy()
+
+
+    seed = p.split('_')[3]
+
+    np.save(f'seed_{seed}_canonical_1',y_post)
+
+list_of_files = sorted(glob.glob('*.json'))
+for p in list_of_files:
+    process_results_file(p)
+
+
+
+
